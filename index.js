6b47c8be0aded0f6e8258273f4cbdbd40f34d707
Jeff Ledin <ledin.jeffrey@gmail.com>
2020-10-12 17:46:02 -0500

    perform basic validation and use NASA Feed API to retrieve asteroids from time range

--- index.js
+++ index.js
@@ -1,30 +1,60 @@
 const axios = require('axios');
 const express = require('express');
+
 const app = express();
 const port = 3000;
 
-const BASE_NASA_NEO_API_URL = 'https://api.nasa.gov/neo/rest/v1/neo/browse';
+app.use(express.json());
+
+const BASE_NASA_NEO_API_URL = 'https://api.nasa.gov/neo/rest/v1/feed';
 
 if (!process.env.NASA_API_KEY) {
   console.error('NASA_API_KEY environment variable not set');
   process.exit(1);
 }
 
+const validateBodyContents = body => {
+  if (!body || !Object.keys(body).length) {
+    throw new Error('Missing required parameters');
+  }
+
+  if (!body.dateStart || !body.dateEnd || !body.within) {
+    throw new Error('One or more parameters are missing');
+  }
+};
+
 app.post('/api/v1/asteroids', async (req, res) => {
+  const { body } = req;
+
   try {
+    validateBodyContents(body);
+
     const { data } = await axios.get(BASE_NASA_NEO_API_URL, {
       params: {
+        start_date: body.dateStart,
+        end_date: body.dateEnd,
         api_key: process.env.NASA_API_KEY
       }
     });
 
-    console.log(data);
+    const { near_earth_objects: nearEarthObjects } = data;
 
-    res.send(data);
+    const asteroids = [];
+    for (const date in nearEarthObjects) {
+      for (const asteroid of nearEarthObjects[date]) {
+        console.log(asteroids);
+
+        // TODO: filter by distance
+        asteroids.push(asteroid.name);
+      }
+    }
+
+    res.send({ asteroids });
   } catch (err) {
     console.log(err);
     res.send({
-      error: true
+      error: true,
+      message: err.message ? err.message : 'Unknown'
     });
   }
 });
