a291c1d0803da77415c4861d3fc63949a5a2b957
Jeff Ledin <ledin.jeffrey@gmail.com>
2020-10-12 16:45:12 -0500

    create initial hello world GET API

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-(async () => {
-  console.log('Hello world');
-})();
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/api/v1', (req, res) => {
+    res.send('Hello from Express API!');
+});
+
+// TODO: handle unsupported routes
+
+app.listen(port, () => {
+  console.log(`API listening on port ${port}`);
+});
+
